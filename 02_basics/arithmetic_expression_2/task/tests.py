c4290e43353ecdff3603ba4602af6dc070874a1d
Patric Salvisberg <patric.salvisberg@gmail.com>
2025-02-03 13:32:26 +0100

    test fix

--- 02_basics/arithmetic_expression_2/task/tests.py
+++ 02_basics/arithmetic_expression_2/task/tests.py
@@ -7,5 +7,5 @@ class PublicTestSuite(TestCase):
     def test_1234(self):
         actual = script.calculateX(1,5,2)
         expected = (-0.438447,-4.56155)
-        self.assertAlmostEqual(expected, actual, 5)
-
+        self.assertAlmostEqual(expected[0], actual[0], 5)
+        self.assertAlmostEqual(expected[1], actual[1], 5)
